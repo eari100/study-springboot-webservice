39fb03dfb241d899dca706427d3f7b79f55926e2
Jaewook <eari100@naver.com>
2021-12-11 18:19:22 +0900

    test: HelloController.helloDto 테스트

--- src/test/java/com/jaewook/studyspringbootwebservice/web/HelloControllerTests.java
+++ src/test/java/com/jaewook/studyspringbootwebservice/web/HelloControllerTests.java
@@ -7,8 +7,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
 
 @WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTests {
@@ -25,4 +25,16 @@ public class HelloControllerTests {
                 .andExpect(content().string(hello));
     }
 
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mockMvc.perform(get("/hello/dto")
+            .param("name", name)
+            .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
 }
