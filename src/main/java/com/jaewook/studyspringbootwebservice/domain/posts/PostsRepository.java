8d28a038b1761a920407bf80af9b64de0f265238
Jaewook <eari100@naver.com>
2021-12-12 19:55:32 +0900

    feat: PostsRepository 추가

---
+++ src/main/java/com/jaewook/studyspringbootwebservice/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.jaewook.studyspringbootwebservice.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
