6b2ab857346956b6d60b4eeee7addcf9a4e4fd21
Jaewook <eari100@naver.com>
2021-12-12 19:56:23 +0900

    test: PostsRepository 테스트

---
+++ src/test/java/com/jaewook/studyspringbootwebservice/domain/posts/PostsRepositoryTests.java
@@ -0,0 +1,46 @@
+package com.jaewook.studyspringbootwebservice.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+public class PostsRepositoryTests {
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanUp() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+        String author = "eari100@naver.com";
+
+        postsRepository.save(Posts.builder()
+            .title(title)
+            .content(content)
+            .author(author)
+            .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertEquals(title, posts.getTitle());
+        assertEquals(content, posts.getContent());
+        assertEquals(author, posts.getAuthor());
+    }
+
+}
