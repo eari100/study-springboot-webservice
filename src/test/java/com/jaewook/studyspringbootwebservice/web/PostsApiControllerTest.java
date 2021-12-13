4d77d3b06f29eae64c33ea26e11d40c81bcd82b0
Jaewook Choi <eari100@naver.com>
2021-12-13 23:40:17 +0900

    test: Posts 등록 API 테스트

---
+++ src/test/java/com/jaewook/studyspringbootwebservice/web/PostsApiControllerTest.java
@@ -0,0 +1,63 @@
+package com.jaewook.studyspringbootwebservice.web;
+
+import com.jaewook.studyspringbootwebservice.domain.posts.Posts;
+import com.jaewook.studyspringbootwebservice.domain.posts.PostsRepository;
+import com.jaewook.studyspringbootwebservice.web.dto.PostsSaveReqDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PostsApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @AfterEach
+    public void tearDown() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void Posts_등록된다() throws Exception {
+        // given
+        String title = "title";
+        String content = "content";
+        String author = "author";
+
+        PostsSaveReqDto reqDto = PostsSaveReqDto.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+         // when
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, reqDto, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+        assertThat(all.get(0).getAuthor()).isEqualTo(author);
+    }
+}
