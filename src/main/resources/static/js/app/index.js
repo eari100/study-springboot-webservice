01bf373511af5a3442753179d6ba5cbec3285eda
Jaewook Choi <eari100@naver.com>
2021-12-15 23:48:28 +0900

    feat: Posts 삭제 ajax 추가

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -6,9 +6,12 @@ const main = {
         })
 
         $('#btn-update').on('click', function () {
-            _this.update();
+            _this.update()
         })
 
+        $('#btn-delete').on('click', function () {
+            _this.delete()
+        })
     },
     save : function () {
         const data = {
@@ -46,6 +49,21 @@ const main = {
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 수정되었습니다.')
+            window.location.href = '/'
+        }).fail(function (error) {
+            alert(JSON.stringify(error))
+        })
+    },
+    delete : function () {
+        const id = $('#id').val()
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.')
             window.location.href = '/'
         }).fail(function (error) {
             alert(JSON.stringify(error))
