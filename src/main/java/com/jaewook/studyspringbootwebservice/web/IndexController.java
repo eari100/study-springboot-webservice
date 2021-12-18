22165c2743daa3c3a7d6405fe8dc20981ed0f836
Jaewook Choi <eari100@naver.com>
2021-12-18 15:15:01 +0900

    refactor: 세션 유저의 정보를 가져오는 로직 @LoginUser 을 사용하도록 리펙토링

--- src/main/java/com/jaewook/studyspringbootwebservice/web/IndexController.java
+++ src/main/java/com/jaewook/studyspringbootwebservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jaewook.studyspringbootwebservice.web;
 
+import com.jaewook.studyspringbootwebservice.config.auth.LoginUser;
 import com.jaewook.studyspringbootwebservice.config.auth.dto.SessionUser;
 import com.jaewook.studyspringbootwebservice.service.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -8,19 +9,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
