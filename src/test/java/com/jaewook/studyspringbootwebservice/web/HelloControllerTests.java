fba57e718c8cc89cda77d69740d756ef85db23bf
Jaewook Choi <eari100@naver.com>
2021-12-20 21:57:47 +0900

    test: HelloControllerTests 수정
    - SecurityConfig.class 관련 설정 scan하지 않도록 수정
    - 테스트별 @WithMockUser 추가

--- src/test/java/com/jaewook/studyspringbootwebservice/web/HelloControllerTests.java
+++ src/test/java/com/jaewook/studyspringbootwebservice/web/HelloControllerTests.java
@@ -1,22 +1,31 @@
 package com.jaewook.studyspringbootwebservice.web;
 
 
+import com.jaewook.studyspringbootwebservice.config.auth.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.is;
 
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+            @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTests {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
@@ -26,6 +35,7 @@ public class HelloControllerTests {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
